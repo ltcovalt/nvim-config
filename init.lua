2d69c52c990c1af1acb6b83cff0b32cde90ca9a6
Luke Covalt <ltcovalt@gmail.com>
2025-10-08 23:23:07 -0400

    add line numbers and auto config reload on save

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- auto reload config on file save
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "init.lua",
   callback = function(args)
